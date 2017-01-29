8c176ef431001fbc723a4afe48a2fa1c471a670b
Raimund Andée <raandree@live.com>
2017-01-29 23:54:43 +0100

    cleanup before full new upload

--- Security2/Exceptions/Exceptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Security2
-{
-    public class RightsConverionException : Exception
-    {
-        public RightsConverionException(string Message) : base(Message) { }
-    }
-}
